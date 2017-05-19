fad60fb0cc90337932164d1815c4670cf17b9397
Jonathan Giroux <giroux.jo@gmail.com>
2017-05-19 10:07:41 +0200

    Add unit tests

--- StealthFactor/premake5.lua
+++ StealthFactor/premake5.lua
@@ -1,6 +1,7 @@
 characterset "Unicode"
 
 defines {
+	"_USE_MATH_DEFINES",
 	"dSINGLE",
 }
 
@@ -178,6 +179,37 @@ project "Sandbox"
 	}
 	links {
 		"Engine",
+	}
+	kind "ConsoleApp"
+	-- rtti "Off"
+	runsWithDependencies()
+
+project "gtest"
+	files {
+		"code/gmock/**",
+		"code/gtest/**",
+	}
+	includedirs {
+		"code",
+		"code/gmock",
+		"code/gtest",
+		"dep/include",
+	}
+	kind "StaticLib"
+	-- rtti "Off"
+
+project "UnitTests"
+	files {
+		"code/unittests/**",
+	}
+	includedirs {
+		"code",
+		"dep/include",
+	}
+	links {
+		"Engine",
+		"gtest",
+		"Platform",
 	}
 	kind "ConsoleApp"
 	-- rtti "Off"
